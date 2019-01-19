da1f4a642e536b5959df63626531893fbd6affed
mirexcool <mirexcool@gmail.com>
2019-01-19 16:55:25 +0200

    asd

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    private Rigidbody2D _rigidbody;
+    private Animator _animatorController;
+    public float speed = 5;
+    public Vector3 move = Vector3.zero;
+    
+    public void movePlayer()
+    {
+        transform.position = Vector3.MoveTowards (transform.position, transform.position + move, speed * Time.deltaTime);
+        MoveAnimation();
+    }
+
+    private void Idle()
+    {
+        _animatorController.Play("Idle");
+    }
+    
+    private void MoveAnimation()
+    {
+        _animatorController.Play("main_character_walk_anim");
+    }
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animatorController = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (move == Vector3.zero)
+            Idle();
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerKeyboardController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKeyboardController : MonoBehaviour {
+
+    public Player Player;
+	
+	private void Start ()
+	{
+    }
+
+    private void Update ()
+    {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Player.transform.position;
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        Player.transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
+        Camera.main.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
+
+        if (Player != null)
+        {
+            Vector3 direction = Vector3.zero;
+            
+            Player.move = direction;
+            if (Input.GetKey(KeyCode.D))
+                Player.move = direction+Vector3.right;
+            if (Input.GetKey(KeyCode.A))
+                Player.move = direction+ Vector3.left;
+            if (Input.GetKey(KeyCode.W))
+                Player.move = direction + Vector3.up;
+            if (Input.GetKey(KeyCode.S))
+                Player.move = direction + Vector3.down;
+            Player.movePlayer();
+        }
+    }
+}
